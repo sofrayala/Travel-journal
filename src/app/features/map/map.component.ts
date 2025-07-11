932206d6ac183c443f4e4cc9e759bef3385d3712
Sofia Ayala <sofiarayala1995@gmail.com>
2025-07-11 17:10:53 +0200

    feat: map: fetchGeocodes(), get coordinates from db countries

--- src/app/features/map/map.component.ts
+++ src/app/features/map/map.component.ts
@@ -9,6 +9,8 @@ import {
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { SupabaseService } from '../../shared/services/supabase.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map',
@@ -20,6 +22,9 @@ export class MapComponent implements OnInit, OnDestroy {
   @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
   map: any;
   private platformId = inject(PLATFORM_ID);
+  private supabaseClient = inject(SupabaseService).supabaseClient;
+  private http = inject(HttpClient);
+  countriesDb: string[] = [];
 
   async ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
@@ -32,8 +37,45 @@ export class MapComponent implements OnInit, OnDestroy {
         zoom: 1,
       });
     }
+
+    //Get country names from trips db
+
+    const { data, error } = await this.supabaseClient
+      .from('trip')
+      .select('name');
+    if (data) {
+      this.countriesDb = data.map((row: any) => row.name);
+    }
+    console.log(this.countriesDb);
+    this.fetchGeocodes();
   }
 
+  fetchGeocodes() {
+    for (const country of this.countriesDb) {
+      const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(
+        country
+      )}&access_token=${environment.MAPBOX_TOKEN}`;
+      this.http.get<any>(url).subscribe({
+        next: (result) => {
+          const feature = result?.features?.[0];
+          // ----------------------------------------
+          // TODO: Mejorar manejo de error
+          // ----------------------------------------
+          if (!feature) {
+            return console.log(`No coordiantes info for this ${country}`);
+          }
+          const [longitude, latitude] =
+            result?.features?.[0].geometry.coordinates;
+          console.log(
+            `Coordinates for ${country}: longitude=${longitude}, latitude=${latitude}`
+          );
+        },
+        error: (err) => {
+          console.error(`Error fetching geocode for ${country}`, err);
+        },
+      });
+    }
+  }
   ngOnDestroy(): void {
     if (this.map) {
       this.map.remove();

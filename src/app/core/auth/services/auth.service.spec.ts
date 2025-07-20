e45bff326a6c1aa727cb6394865d48e0c0c6af3e
Sofia Ayala <sofiarayala1995@gmail.com>
2025-07-20 11:12:28 +0200

    authService tests

--- src/app/core/auth/services/auth.service.spec.ts
+++ src/app/core/auth/services/auth.service.spec.ts
@@ -13,4 +13,12 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have a signOut method', () => {
+    expect(typeof service.signOut).toBe('function');
+  });
+
+  it('should have a session method', () => {
+    expect(typeof service.session).toBe('function');
+  });
 });

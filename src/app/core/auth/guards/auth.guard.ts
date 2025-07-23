aa7717e54ff3daa36e7126824a713b016f5484a3
Sofia Ayala <sofiarayala1995@gmail.com>
2025-07-23 11:16:43 +0200

    core:auth:guards: refactored auth.guard

--- src/app/core/auth/guards/auth.guard.ts
+++ src/app/core/auth/guards/auth.guard.ts
@@ -2,12 +2,10 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthServiceService } from '../services/auth.service';
 
-const routerInjection = () => inject(Router);
-
 const authService = () => inject(AuthServiceService);
 
 export const authGuard: CanActivateFn = async () => {
-  const router = routerInjection();
+  const router = inject(Router);
 
   const { data } = await authService().session();
 
@@ -15,5 +13,5 @@ export const authGuard: CanActivateFn = async () => {
     router.navigateByUrl('/log-in');
   }
 
-  return !!data.session;
+  return true;
 };
